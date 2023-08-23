ab0fd64f3566d665dac21f3818239d1073eb1691
GianmarcoBramucci <giangian797@gmail.com>
2023-08-23 10:40:05 +0200

    fix script

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
-const button= document.getElementById('btn');
-const griglia= document.getElementById('gril');
-const cellTot=100;
-button.addEventListener('click',function(){
-    genereteGrill(cellTot,griglia);
+const button = document.getElementById('btn');
+const griglia = document.getElementById('gril');
+const cellTot = 100;
+
+button.addEventListener('click', function () {
+    genereteGrill(cellTot, griglia);
 })
-function genereteGrill(cellNum,conteiner){
-    // conteiner.innerHTML='';
-    for(let i=0;i<cellNum;i++){
-        genereteCell(conteiner,i);
+
+function genereteGrill(cellNum, conteiner) {
+    conteiner.innerHTML = '';
+    for (let i = 1; i <= cellNum; i++) {
+        genereteCell(conteiner, i);
     }
 }
-function genereteCell(conteiner,index){
-    const cell= document.createElement('div');
-    cell.innerText= index;
+
+function genereteCell(conteiner, index) {
+    const cell = document.createElement('div');
+    cell.innerText = index;
     cell.classList.add('cell');
-    console.log(cell,'hello word')
-    // conteiner.append('cell');
+    conteiner.append(cell);
+    cell.addEventListener('click', function () {
+        cell.classList.add('cell-blu');
+        console.log(cell.innerHTML)
+    })
 }
